28408c7242ae55e8d661c87769dddda0d7dd65b7
ikramhasib007 <ikramhasib007@gmail.com>
2023-02-18 17:11:31 +0600

    client boilerplate prepared

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html
+      dir='ltr'
+      lang='en'
+      className='h-full antialiased text-gray-900 scroll-smooth'
+    >
+      <Head>
+        <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+        <meta charSet='utf-8' />
+      </Head>
+      <body className='min-h-screen h-full bg-gray-100 font-sans text-base font-normal tracking-normal'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
