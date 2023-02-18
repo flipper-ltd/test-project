66d3f751a5cc26dd5f29bae5613da76a9d35a99e
ikramhasib007 <ikramhasib007@gmail.com>
2023-02-18 19:57:42 +0600

    client files list ui created

---
+++ client/src/routes/home/FilesList.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+type FilesListProps = {
+  create: Function;
+};
+
+const people = [
+  {
+    id: `${new Date().getTime()}`,
+    name: "Lindsay Walton",
+    mimetype: "Front-end Developer",
+    size: "lindsay.walton@example.com",
+  },
+  // More file...
+];
+
+const FilesList: React.FC<FilesListProps> = ({ create }) => {
+  return (
+    <div className='shadow sm:overflow-hidden sm:rounded-md'>
+      <div className='space-y-6 bg-white py-6 px-4 sm:p-6'>
+        <div className='sm:flex sm:items-center'>
+          <div className='sm:flex-auto'>
+            <h1 className='text-xl font-semibold text-gray-900'>Files List</h1>
+            <p className='mt-2 text-sm text-gray-700'>
+              A list of all the files in your account including their name,
+              mimtype, and size.
+            </p>
+          </div>
+          <div className='mt-4 sm:mt-0 sm:ml-16 sm:flex-none'>
+            <button
+              type='button'
+              className='block rounded-md bg-indigo-600 py-1.5 px-3 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              onClick={() => create()}
+            >
+              Add files
+            </button>
+          </div>
+        </div>
+        <div className='mt-8 flow-root'>
+          <div className='-my-2 -mx-6 overflow-x-auto lg:-mx-8'>
+            <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
+              <table className='min-w-full divide-y divide-gray-300'>
+                <thead>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='py-3.5 pl-6 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0'
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope='col'
+                      className='py-3.5 px-3 text-left text-sm font-semibold text-gray-900'
+                    >
+                      MIME type
+                    </th>
+                    <th
+                      scope='col'
+                      className='py-3.5 px-3 text-left text-sm font-semibold text-gray-900'
+                    >
+                      Size
+                    </th>
+                    <th
+                      scope='col'
+                      className='relative py-3.5 pl-3 pr-6 sm:pr-0'
+                    >
+                      <span className='sr-only'>Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className='divide-y divide-gray-200'>
+                  {people.map((person) => (
+                    <tr key={person.id}>
+                      <td className='whitespace-nowrap py-4 pl-6 pr-3 text-sm font-medium text-gray-900 sm:pl-0'>
+                        {person.name}
+                      </td>
+                      <td className='whitespace-nowrap py-4 px-3 text-sm text-gray-500'>
+                        {person.mimetype}
+                      </td>
+                      <td className='whitespace-nowrap py-4 px-3 text-sm text-gray-500'>
+                        {person.size}
+                      </td>
+                      <td className='relative whitespace-nowrap py-4 pl-3 pr-6 text-right text-sm font-medium sm:pr-0'>
+                        <a
+                          href='#'
+                          className='text-indigo-600 hover:text-indigo-900'
+                        >
+                          Edit<span className='sr-only'>, {person.name}</span>
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilesList;
