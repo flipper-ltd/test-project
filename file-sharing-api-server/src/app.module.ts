ba03ef7b70acdd814b239c828c6e9b224e8f0fa1
ikramhasib007 <ikramhasib007@gmail.com>
2023-02-19 11:33:49 +0600

    files enpoint just added

--- file-sharing-api-server/src/app.module.ts
+++ file-sharing-api-server/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configValidationSchema } from './config.schema';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: configValidationSchema,
     }),
+    FilesModule,
   ],
 })
 export class AppModule {}
